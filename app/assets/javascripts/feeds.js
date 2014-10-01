eb252518ac26282c960d7efc1c1e2dae71e8c9b7
postliminary <thomas@postliminary.com>
2014-10-01 17:25:28 +0900

    Tweak to UI, add sticky sidebars

--- app/assets/javascripts/feeds.js
+++ app/assets/javascripts/feeds.js
@@ -5,8 +5,8 @@ var feeds = {
                 $("#recent-entries").html(data.partial_html)
 
                 // Slowly extend polling to match update freq
-                // 2 - 30s
-                var new_interval = Math.min(Math.max(data.last_updated, 2000), 30000)
+                // 2 - 10s
+                var new_interval = Math.min(Math.max(data.last_updated, 2000), 10000)
                 feeds.update(id, new_interval);
             })
         }, interval);
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,7 +6,7 @@ var home = {
 
                 // Slowly extend polling to match update freq
                 // 2 - 30s
-                var new_interval = Math.min(Math.max(data.last_updated, 2000), 30000)
+                var new_interval = Math.min(Math.max(data.last_updated, 2000), 10000)
                 home.update(new_interval);
             })
         }, interval);
